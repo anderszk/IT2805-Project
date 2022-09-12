df4e8cc4818a1b66f7a1816cdf8b4c8974557478
anderszk <anderszk@stud.ntnu.no>
2022-09-12 10:51:11 +0200

    hyh

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ window.onscroll = function() {
 };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("navbar").style.backgroundColor= "#161616";
         document.getElementById("navbar").style.borderColor= "transparent";
         document.getElementById("navbar").style.borderColor= "#BD4848";
@@ -11,4 +11,32 @@ function scrollFunction() {
         document.getElementById("navbar").style.transition= "300ms ease-out";
         document.getElementById("navbar").style.backgroundColor= "transparent";
     }
+}
+
+function formValidation() {
+    let name = document.forms["contact-form"]["fname"].value;
+    let phone = document.forms["contact-form"]["phone"].value;
+    let mail = document.forms["contact-form"]["mail"].value
+    if (!name_validator()) {
+        
+    }
+    else if (!phone_validator()) {
+
+    }
+    else if (!mail_validator()) {
+
+    }
+    else{return;} //else submit form
+}
+
+function name_validator(name) {
+    //only char
+}
+
+function phone_validator(phone) {
+    //length 8 only char
+}
+
+function mail_validator(mail) {
+    //right .com and have @
 }
