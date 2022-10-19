69dcba2caddb7ae2913b84c5728eb13a9fcc84be
Engum <thomasjengum@gmail.com>
2022-10-19 10:21:26 +0200

    f

--- src/js/contactInfo.js
+++ src/js/contactInfo.js
@@ -1,7 +1,11 @@
-opencontactinfo();{
+function opencontactinfo();{
     let div = document.createElement("div");
     let p = document.createElement("p");
     div.prepend("Some text", p);
     
     console.log(div.childNodes); // NodeList [ #text "Some text", <p> ]
 }
+
+function myFunction(p1, p2) {
+    return p1 * p2;
+  }
